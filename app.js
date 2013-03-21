f6391d78d5c72da693e8630f434777400ca1bf39
webdevoloper <webdevolop@yandex.ru>
2013-03-21 19:15:21 +0400

    site

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ app.configure(function() {
 app.get('/', function(req, res){          // Обрабатываем запрос корневой страницы "/"
     res.render('index.html');
 });
+
+/*
 app.get('/portfolio', function(req, res){ // Обрабатываем запрос страницы "/portfolio"
     res.render('portfolio.html');
 });
+*/
 
 var port = process.env.PORT || 5000;       
 app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
